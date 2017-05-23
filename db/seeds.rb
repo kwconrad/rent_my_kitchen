2735babfbb373a99be6c70a5e43b4867a4d124b2
joao valerio <joaocvalerio@gmail.com>
2017-05-23 16:31:29 +0100

    added somes seeds to test the index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user2 = User.create(email:"Amanda@gmail.com", password:"1234563")
+
+kit1 = Kitchen.new(name: "Joao's Kitchen", description:"Lovely Kitchen", street_address:"Ruazdo Embaxador nº3", city:"Lisbon", postal_code:"1400-253", country:"Portugal", capacity:5, price_per_hour:5)
+kit2 = Kitchen.new(name: "Amanda's Kitchen", description:"Perfect Kitchen", street_address:"Rua das Asturias nº10", city:"S.Paulo", postal_code:"3253", country:"Brazil", capacity:10, price_per_hour:5)
+
+kit2.user = user2
+
+kit1.save
+kit2.save
