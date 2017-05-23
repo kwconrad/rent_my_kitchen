c2c256925537e84775646cb69e0b3d885c0bc3a0
Luca Nioi <lucahidekinioi@gmail.com>
2017-05-23 18:35:57 +0100

    fixed a minor issue with kitchen's destory dependency with amenities

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -1,5 +1,5 @@
 class Amenity < ApplicationRecord
-  has_many :kitchen_amenities
+  has_many :kitchen_amenities, dependent: :destroy
   has_many :kitchens, through: :kitchen_amenities
 
   validates :name, :category, presence: true
